29521fed4538560272ee7c32551add9b7f2e37f6
leor8 <leo442183205@gmail.com>
2018-10-03 21:51:31 -0700

    now when in main activity, if device is covered by hand, a beep sound will appear and if the device is flat on surface, it vibrates

--- app/src/main/java/com/example/leoruan/leoruan_a2/MainActivity.java
+++ app/src/main/java/com/example/leoruan/leoruan_a2/MainActivity.java
@@ -5,24 +5,36 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.ToneGenerator;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     RecyclerView recyclerView;
     List<Sensor> sensor_list;
     Sensor lSensor;
+    Sensor accSensor;
+    MediaPlayer mp = null;
 
     SensorManager mySensorManager;
 
     Boolean played = false;
+    Boolean vibrated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensor_list = mySensorManager.getSensorList(Sensor.TYPE_ALL);
 
         lSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        accSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         recyclerView.setAdapter(new MyAdapter(this, sensor_list));
     }
@@ -46,10 +59,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(type == Sensor.TYPE_LIGHT) {
             float sensor_val = event.values[0];
             if(sensor_val == 0 && !played) {
-                ToneGenerator beep = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
-//                beep.startTone(ToneGenerator.TONE_CDMA_PIP,150);
-//                beep.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 150);
 
+                try {
+                    mp = MediaPlayer.create(this, R.raw.beep);
+                    played = true;
+                    mp.start();
+                } catch (Exception e){
+                    Toast.makeText(this, "There is an error with playing sound", Toast.LENGTH_SHORT).show();
+                }
+
+            } else if (sensor_val > 0) {
+
+                played = false;
+            }
+        }
+
+        if(type == Sensor.TYPE_ACCELEROMETER){
+            double x = event.values.clone()[0];
+            double y = event.values.clone()[1];
+            double z = event.values.clone()[2];
+            float norm_Of_g =(float) Math.sqrt(x * x + y * y + z * z);
+
+            // Normalize the accelerometer vector
+            x = (x / norm_Of_g);
+            y = (y / norm_Of_g);
+            z = (z / norm_Of_g);
+            int inclination = (int) Math.round(Math.toDegrees(Math.acos(z)));
+            Log.d("DEBUG555", ""+inclination);
+
+            if (inclination < 10 || inclination > 175)
+            {
+                if(!vibrated) {
+                    Vibrator v = (Vibrator) getSystemService(this.VIBRATOR_SERVICE);
+                    v.vibrate(500);
+                    vibrated = true;
+                }
+            } else {
+                vibrated = false;
             }
         }
     }
@@ -63,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         mySensorManager.registerListener((SensorEventListener) this, lSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mySensorManager.registerListener((SensorEventListener) this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
